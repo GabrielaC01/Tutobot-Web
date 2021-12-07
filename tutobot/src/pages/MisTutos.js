677c7c9c2529cdf71d8db02febf177c182ffa7f8
Gabriela <gabriela.colque.u@uni.pe>
2021-12-07 00:17:17 -0500

    barra

--- tutobot/src/pages/MisTutos.js
+++ tutobot/src/pages/MisTutos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CardTuto from "../components/CardTuto";
+import CardTuto from '../components/CardMisTutos';
 
 function MisTutos() {
   return (
@@ -8,19 +8,23 @@ function MisTutos() {
         <CardTuto
           title="Cálculo de determinantes"
           text="En este tutorial, aprenderás a usar las diversas técnicas que se emplean para el cálculo de determinantes"
+          done="50"
         />
         <CardTuto
           title="Cálculo de límites"
           text="En este tutorial, aprenderás a usar las diversas técnicas que se emplean para el cálculo de límites"
+          done="70"
         />
         <CardTuto
           title="Biología"
           text="En este tutorial, aprenderás los conceptos básicos acerca de las ciencias biológicas"
+          done="80"
         />
 
         <CardTuto
           title="Cálculo de probabilidades"
           text="En este tutorial, aprenderás a usar las diversas técnicas que se emplean para el cálculo de probabilidades"
+          done="10"
         />
       </div>
     </div>
